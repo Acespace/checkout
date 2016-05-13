2d3565cbbf5f62671833ac2c0f13d738b0278163
Acespace <alston.clark17@gmail.com>
2016-05-12 20:19:03 -0400

    looks better, working on js

--- angular_project/js/mainController.js
+++ angular_project/js/mainController.js
@@ -2,7 +2,22 @@
 
 
 app.controller('MainController', ['$scope', function($scope) { 
-  $scope.title = 'This Month\'s Bestsellers'; 
-  $scope.promo = 'The most popular books this month.';
+  $scope.title = 'ShopCircle'; 
+  $scope.promo = 'Invite your friends and start your shopping circle.';
+
+  $scope.user = {
+
+  	name: "ace",
+  	email: "ace",
+  	password: "000"
+
+  };
+
+ $scope.logins = [];
+    $scope.login = function () {
+        $scope.logins.push($scope.user.firstName + " was logged in.");
+    };
+
+
 
 }]);
