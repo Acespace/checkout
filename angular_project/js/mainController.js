e626dff70ceda2e1b5a5e8ef06b293ad4a69b0da
Acespace <alston.clark17@gmail.com>
2016-05-18 22:39:37 -0400

    user signup complete

--- angular_project/js/mainController.js
+++ angular_project/js/mainController.js
@@ -5,19 +5,33 @@ app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'ShopCircle'; 
   $scope.promo = 'Invite your friends and start your shopping circle.';
 
-  $scope.user = {
+$scope.userData = {
 
-  	name: "ace",
-  	email: "ace",
-  	password: "000"
+	name: '',
+	email: '',
+	password:''
+	
+};
 
-  };
+$scope.saveData = function(){
 
- $scope.logins = [];
-    $scope.login = function () {
-        $scope.logins.push($scope.user.firstName + " was logged in.");
-    };
+		
+		window.alert("just created user");
+
+		var ref = new Firebase("https://torrid-heat-3055.firebaseio.com/");
+	 	ref.createUser({email: $scope.userData.email, 
+	 		password: $scope.userData.password},
+	 		function(error, userData) {
+  			if (error){
+  				console.log(error);
+  			}
 
 
+	 		});
+
+	};
+
+	 
 
 }]);
+
